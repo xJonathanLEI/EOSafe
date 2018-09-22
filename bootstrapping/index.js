bb68540cb49b31a25cc9ac7cf73b078bc4251abc
Jonathan LEI <xJonathan@outlook.com>
2018-09-22 20:55:43 +0100

    Added skeleton for boostrapping scripts

---
+++ bootstrapping/index.js
@@ -0,0 +1,12 @@
+const Eos = require("eosjs");
+const eos = Eos({
+    httpEndpoint: "http://127.0.0.1:8888",
+    chainId: "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f",
+    keyProvider: ""
+});
+
+async function run() {
+
+}
+
+run();
