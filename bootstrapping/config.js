c9f2860a72a03e1487bdbb6104f673a445a0dc4f
Jonathan LEI <xJonathan@outlook.com>
2018-09-22 21:34:37 +0100

    Added EOS system account initialization

---
+++ bootstrapping/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    sysPrivKey: "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+};
--- bootstrapping/index.js
+++ bootstrapping/index.js
@@ -1,12 +1,14 @@
+const config = require("./config");
+
 const Eos = require("eosjs");
 const eos = Eos({
     httpEndpoint: "http://127.0.0.1:8888",
     chainId: "cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f",
-    keyProvider: ""
+    keyProvider: config.sysPrivKey
 });
 
 async function run() {
-
+    await require("./system").initSys(eos);
 }
 
 run();
