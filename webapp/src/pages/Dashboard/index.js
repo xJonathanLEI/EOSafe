e2d31650af5a59ba28b4ed61316937f86340868a
Jonathan LEI <xJonathan@outlook.com>
2018-09-23 01:39:18 +0100

    Added change limit modal to department dashboard page

--- webapp/src/pages/Dashboard/index.js
+++ webapp/src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Col, Row, Divider } from 'antd';
+import { Card, Col, Row, Divider, Modal, Input } from 'antd';
 
 import ExpenditureDisplay from "../../components/ExpenditureDisplay";
 
@@ -8,9 +8,30 @@ class Dashborad extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            changeAllowanceModal: false,
             key: 'tab1',
             noTitleKey: 'app',
         };
+    }
+
+    showModal = () => {
+        this.setState({
+            changeAllowanceModal: true,
+        });
+    }
+
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            changeAllowanceModal: false,
+        });
+    }
+
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            changeAllowanceModal: false,
+        });
     }
 
     render() {
@@ -20,7 +41,7 @@ class Dashborad extends Component {
                 <Card
                     style={{ width: '100%' }}
                     title="Allowance"
-                    extra={<a href="#">Apply to Change</a>}
+                    extra={<a href="#" onClick={this.showModal}>Apply to Change</a>}
                     activeTabKey={this.state.key}
                     onTabChange={(key) => { this.onTabChange(key, 'key'); }}
                 >
@@ -124,6 +145,59 @@ class Dashborad extends Component {
                         </Card>
                     </Col>
                 </Row>
+
+                <Modal
+                    title="Change Department Allowance"
+                    visible={this.state.changeAllowanceModal}
+                    okText="Submit"
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <Row gutter={16}>
+                        <Col span={10} style={{ textAlign: "center" }}>
+                            <p>Current Allowance:</p>
+                        </Col>
+                        <Col span={10} style={{ textAlign: "right" }}>
+                            <p>1000.0000</p>
+                        </Col>
+                        <Col span={4}>
+                            <p>EOS</p>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={10} style={{ textAlign: "center" }}>
+                            <p>Allocated Allowance:</p>
+                        </Col>
+                        <Col span={10} style={{ textAlign: "right" }}>
+                            <p>800.0000</p>
+                        </Col>
+                        <Col span={4}>
+                            <p>EOS</p>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col span={10} style={{ textAlign: "center" }}>
+                            <p>Allowance Used:</p>
+                        </Col>
+                        <Col span={10} style={{ textAlign: "right" }}>
+                            <p>400.0000</p>
+                        </Col>
+                        <Col span={4}>
+                            <p>EOS</p>
+                        </Col>
+                    </Row>
+                    <Row gutter={16} style={{ display: "flex", alignItems: "center" }}>
+                        <Col span={10} style={{ textAlign: "center" }}>
+                            <p style={{ margin: 0 }}>New Allowance:</p>
+                        </Col>
+                        <Col span={10} style={{ textAlign: "right" }}>
+                            <Input placeholder="" />
+                        </Col>
+                        <Col span={4}>
+                            <p style={{ margin: 0 }}>EOS</p>
+                        </Col>
+                    </Row>
+                </Modal>
             </div>
         );
     }
