5e33c7b877d9c2bfe9bc5938f8c314def3fdbe3c
Jonathan LEI <xJonathan@outlook.com>
2018-09-23 02:44:04 +0100

    Fixed router redirection

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route,
-    Switch
+    Switch,
+    withRouter
 } from 'react-router-dom'
 import { Layout, Menu } from 'antd';
 import './App.css';
@@ -26,36 +27,34 @@ class App extends Component {
 
     render() {
         return (
-            <Router>
-                <Layout className="layout">
-                    <Header>
-                        <div className="logo" />
-                        <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['1']}
-                            style={{ lineHeight: '64px' }}>
-                            <Menu.Item key="1" onClick={()=>window.location="/"}>Dashboard</Menu.Item>
-                            <Menu.Item key="2" onClick={()=>window.location="/placeholder"}>Placeholder</Menu.Item>
-                        </Menu>
-                    </Header>
-                    <Content style={{ padding: '0 50px' }}>
-                        <div style={{ background: '#fff', marginTop: 24, padding: 24, minHeight: 280 }}>
-                            <Switch>
-                                <Route exact path="/" component={dashboard} />
-                                <Route exact path="/placeholder" component={placeholderBoard} />
-                            </Switch>
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>
-                        Created by EOSafe
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                        style={{ lineHeight: '64px' }}>
+                        <Menu.Item key="1" onClick={() => { this.props.history.push("/") }}>Dashboard</Menu.Item>
+                        <Menu.Item key="2" onClick={() => { this.props.history.push("/placeholder") }}>Placeholder</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <div style={{ background: '#fff', marginTop: 24, padding: 24, minHeight: 280 }}>
+                        <Switch>
+                            <Route exact path="/" component={dashboard} />
+                            <Route exact path="/placeholder" component={placeholderBoard} />
+                        </Switch>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Created by EOSafe
                     </Footer>
-                </Layout>
-            </Router>
+            </Layout>
         );
     }
 }
 
 
 
-export default App;
+export default withRouter(App);
